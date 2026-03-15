673008597c8eca3f8533324b2976977ecfc737c7
BM Arafat <alarafat.steadfast@gmail.com>
2026-03-15 04:21:02 +0000

    popup solution

--- js/popup.js
+++ js/popup.js
@@ -2,53 +2,55 @@ function buildPopup(hub, lat, lng) {
   return `
     <div class="hub-popup">
 
-      <h2 class="title">Hub Details</h2>
+      <div class="hub-popup-body">
 
-      <div class="box"><b>Name:</b> ${hub.name || ""}</div>
+        <h2 class="title">Hub Details</h2>
 
-      <div class="box"><b>Address:</b> ${hub.address || ""}</div>
+        <div class="box"><b>Name:</b> ${hub.name || ""}</div>
 
-      <div class="grid2">
-        <div class="box"><b>Hub ID:</b> ${hub.hub_id || ""}</div>
-        <div class="box"><b>Zone:</b> ${hub.zone || ""}</div>
+        <div class="box"><b>Address:</b> ${hub.address || ""}</div>
+
+        <div class="grid2">
+          <div class="box"><b>Hub ID:</b> ${hub.hub_id || ""}</div>
+          <div class="box"><b>Zone:</b> ${hub.zone || ""}</div>
+        </div>
+
+        <div class="grid2">
+          <div class="box"><b>District:</b> ${hub.district || ""}</div>
+          <div class="box"><b>Division:</b> ${hub.division || ""}</div>
+        </div>
+
+        <div class="box">
+          <b>Coordinates:</b> ${lat}, ${lng}
+        </div>
+
+        <hr>
+
+        <div class="section">Contact Details</div>
+
+        <div class="grid2">
+          <div class="box"><b>Hub IP:</b> ${hub.hub_ip || ""}</div>
+          <div class="box"><b>Hub Phone:</b> ${hub.hub_phone || ""}</div>
+        </div>
+
+        <div class="grid2">
+          <div class="box"><b>Manager:</b> ${hub.manager || ""}</div>
+          <div class="box"><b>Phone:</b> ${hub.manager_phone || ""}</div>
+        </div>
+
+        <div class="grid2">
+          <div class="box"><b>Asst. Manager:</b> ${hub.assistant_manager || ""}</div>
+          <div class="box"><b>Phone:</b> ${hub.assistant_manager_phone || ""}</div>
+        </div>
+
+        <div class="grid2">
+          <div class="box"><b>Hub Asst:</b> ${hub.hub_assistant || ""}</div>
+          <div class="box"><b>Phone:</b> ${hub.hub_assistant_phone || ""}</div>
+        </div>
+
       </div>
 
-      <div class="grid2">
-        <div class="box"><b>District:</b> ${hub.district || ""}</div>
-        <div class="box"><b>Division:</b> ${hub.division || ""}</div>
-      </div>
-
-      <div class="box">
-        <b>Coordinates:</b> ${lat}, ${lng}
-      </div>
-
-      <hr>
-
-      <div class="section">Contact Details</div>
-
-      <div class="grid2">
-        <div class="box"><b>Hub IP:</b> ${hub.hub_ip || ""}</div>
-        <div class="box"><b>Hub Phone:</b> ${hub.hub_phone || ""}</div>
-      </div>
-
-      <div class="grid2">
-        <div class="box"><b>Manager:</b> ${hub.manager || ""}</div>
-        <div class="box"><b>Phone:</b> ${hub.manager_phone || ""}</div>
-      </div>
-
-      <div class="grid2">
-        <div class="box"><b>Asst. Manager:</b> ${hub.assistant_manager || ""}</div>
-        <div class="box"><b>Phone:</b> ${hub.assistant_manager_phone || ""}</div>
-      </div>
-
-      <div class="grid2">
-        <div class="box"><b>Hub Asst:</b> ${hub.hub_assistant || ""}</div>
-        <div class="box"><b>Phone:</b> ${hub.hub_assistant_phone || ""}</div>
-      </div>
-
-      <hr>
-
-      <div style="text-align:center;">
+      <div class="hub-popup-footer">
         <button class="direction-btn" onclick="openDirections(${lat},${lng})">
           📍 Get Directions
         </button>
